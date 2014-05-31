92ff51274fe58313129ec892b2ff6ccb6a92efd6
sorressean <tyler@tysdomain.com>
2014-05-31 13:33:18 +0000

    Started work on documenting code, added doxygen file.

--- src/affect.h
+++ src/affect.h
@@ -3,11 +3,21 @@
 #include "conf.h"
 #include "attribute.h"
 
+/**
+This class is used to apply affects to any living object.
+It can hold a duration (number of game ticks) as well as the attributes to apply.
+\todo Allow affects to store multiple attributes.
+\todo allow an ID or some pointer to be set so that affects and their timers can be destroyed when a living leaves the game.
+\todo add affect removal code on game leave.
+*/
 class Affect
 {
     Attribute* _attribute;
     int _duration;
 public:
+/**
+Creates an affect, providing an attribute and the duration (in game ticks).
+*/
     Affect(int duration, Attribute* attr);
     Affect();
     ~Affect();
