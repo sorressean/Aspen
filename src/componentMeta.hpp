e337ea84c0d98c7e35bd017222cf5e6ca71633f2
sorressean <tyler@tysdomain.com>
2015-01-28 00:59:42 +0000

    Formatted hpp files.

--- src/componentMeta.hpp
+++ src/componentMeta.hpp
@@ -19,12 +19,12 @@
 class IComponentMeta
 {
 public:
-virtual ~IComponentMeta() { }
-virtual Component* Create() = 0;
+    virtual ~IComponentMeta() { }
+    virtual Component* Create() = 0;
     virtual void Initialize() = 0;
-virtual std::string GetName() const = 0;
-virtual     BOOL AddDependency(const std::string &dependency) = 0;
-virtual     void GetDependencies(std::vector<std::string>* out) const = 0;
+    virtual std::string GetName() const = 0;
+    virtual     BOOL AddDependency(const std::string &dependency) = 0;
+    virtual     void GetDependencies(std::vector<std::string>* out) const = 0;
     virtual OLCGROUP GetOlcGroup() const = 0;
     virtual void Serialize(TiXmlElement* root) = 0;
     virtual void Deserialize(TiXmlElement* root) = 0;
@@ -37,123 +37,124 @@ class ComponentMeta:public IComponentMeta
     std::vector <std::string> _dependencies; //component dependencies.
 public:
     ComponentMeta(const std::string &name)
-{
-_name = name;
-}
-~ComponentMeta()
-{
-}
+    {
+        _name = name;
+        events.RegisterEvent("OnInitialize", new Event());
+    }
+    ~ComponentMeta()
+    {
+    }
     void SetName(const std::string &name)
-{
-_name = name;
-}
+    {
+        _name = name;
+    }
     std::string GetName() const
-{
-return _name;
-}
+    {
+        return _name;
+    }
     /*
     *Checks to see if the specified dependency is located in the dependencies list.
     *Param: [in] the name of the dependency.
     *Return: True if the dependency exists in the dependencies list, false otherwise.
     */
     BOOL DependencyExists(const std::string &name) const
-{
-    std::vector<std::string>::const_iterator it, itEnd;
+    {
+        std::vector<std::string>::const_iterator it, itEnd;
 
-    itEnd = _dependencies.end();
-    for (it = _dependencies.begin(); it != itEnd; ++it)
-        {
-            if ((*it) == name)
-                {
-                    return true;
-                }
-        }
+        itEnd = _dependencies.end();
+        for (it = _dependencies.begin(); it != itEnd; ++it)
+            {
+                if ((*it) == name)
+                    {
+                        return true;
+                    }
+            }
 
-    return false;
-}
+        return false;
+    }
 
     /*
     *Will add the specified dependency to the list.
     *Param: [in] The name of the component to add as a dependency.
     */
     BOOL AddDependency(const std::string &dependency)
-{
-    if (DependencyExists(dependency))
-        {
-            return false;
-        }
+    {
+        if (DependencyExists(dependency))
+            {
+                return false;
+            }
 
-    _dependencies.push_back(dependency);
-    return true;
-}
+        _dependencies.push_back(dependency);
+        return true;
+    }
 
     /*
     *Returns a pointer to a vector of dependency names.
     *Return: a pointer to the vector of strings containing the names of the dependencies associated with the component.
     */
     void GetDependencies(std::vector<std::string>* out) const
-{
-    std::copy(_dependencies.begin(), _dependencies.end(), out->end());
-}
+    {
+        std::copy(_dependencies.begin(), _dependencies.end(), out->end());
+    }
 
     /*
     *Called to create an instance of the component.
     *Param: [in] a pointer to the instance of the ComponentMeta object responsible for the component.
     */
     virtual T* Create()
-{
-return new T(this);
-}
+    {
+        return new T(this);
+    }
 
     /*
     *Does all the initialization for a component once it's added
     *to the component factory.
     */
     virtual void Initialize()
-{
-}
+    {
+    }
 
     virtual OLCGROUP GetOlcGroup() const
-{
-    return OLCGROUP::NONE;
-}
+    {
+        return OLCGROUP::NONE;
+    }
     virtual void Serialize(TiXmlElement* root)
-{
-    TiXmlElement* ent = new TiXmlElement("cmeta");
-    TiXmlElement* dependencies = new TiXmlElement("dependencies");
-    TiXmlElement* dependency = NULL;
-    std::vector<std::string>::iterator it, itEnd;
+    {
+        TiXmlElement* ent = new TiXmlElement("cmeta");
+        TiXmlElement* dependencies = new TiXmlElement("dependencies");
+        TiXmlElement* dependency = NULL;
+        std::vector<std::string>::iterator it, itEnd;
 
-    ent->SetAttribute("name", _name.c_str());
+        ent->SetAttribute("name", _name.c_str());
 
-    itEnd = _dependencies.end();
-    for (it = _dependencies.begin(); it != itEnd; ++it)
-        {
-            dependency = new TiXmlElement("dependency");
-            dependency->SetAttribute("name", (*it).c_str());
-            dependencies->LinkEndChild(dependency);
-        }
-    ent->LinkEndChild(dependencies);
+        itEnd = _dependencies.end();
+        for (it = _dependencies.begin(); it != itEnd; ++it)
+            {
+                dependency = new TiXmlElement("dependency");
+                dependency->SetAttribute("name", (*it).c_str());
+                dependencies->LinkEndChild(dependency);
+            }
+        ent->LinkEndChild(dependencies);
 
-    root->LinkEndChild(ent);
-}
+        root->LinkEndChild(ent);
+    }
     virtual void Deserialize(TiXmlElement* root)
-{
-    TiXmlNode* node = NULL;
-    TiXmlElement* dependency = NULL;
-    TiXmlElement *dependencies = NULL;
-    _name = root->Attribute("name");
+    {
+        TiXmlNode* node = NULL;
+        TiXmlElement* dependency = NULL;
+        TiXmlElement *dependencies = NULL;
+        _name = root->Attribute("name");
 
-    node = root->FirstChild("dependencies");
-    if (node)
-        {
-            dependencies = node->ToElement();
-            for (node = dependencies->FirstChild(); node; node = node->NextSibling())
-                {
-                    dependency = node->ToElement();
-                    AddDependency(dependency->Attribute("name"));
-                }
-        }
-}
+        node = root->FirstChild("dependencies");
+        if (node)
+            {
+                dependencies = node->ToElement();
+                for (node = dependencies->FirstChild(); node; node = node->NextSibling())
+                    {
+                        dependency = node->ToElement();
+                        AddDependency(dependency->Attribute("name"));
+                    }
+            }
+    }
 };
 #endif
--- src/serializationHelpers.hpp
+++ src/serializationHelpers.hpp
@@ -4,37 +4,37 @@
 template <class O, class C, class I>
 BOOL SerializeList(const std::string& parent, const std::string& nodename, TiXmlElement* root, C& container)
 {
-I it, itEnd;
-TiXmlElement* pnode = new TiXmlElement(parent.c_str());
+    I it, itEnd;
+    TiXmlElement* pnode = new TiXmlElement(parent.c_str());
 
-itEnd = container.end();
-for (it = container.begin(); it != itEnd; ++it)
-{
-(*it)->Serialize(pnode);
-}
-root->LinkEndChild(pnode);
-return true;
+    itEnd = container.end();
+    for (it = container.begin(); it != itEnd; ++it)
+        {
+            (*it)->Serialize(pnode);
+        }
+    root->LinkEndChild(pnode);
+    return true;
 }
 
 template <class O, class C>
 BOOL DeserializeList(TiXmlElement* root, const std::string &parent, C& objects)
 {
-TiXmlNode* node = NULL;
-TiXmlElement* element = NULL;
-TiXmlElement* pnode = NULL;
-O* obj = NULL;
+    TiXmlNode* node = NULL;
+    TiXmlElement* element = NULL;
+    TiXmlElement* pnode = NULL;
+    O* obj = NULL;
 
-node = root->FirstChild(parent.c_str());
-if (node)
-{
-pnode = node->ToElement();
-for (node = pnode->FirstChild(); node; node=node->NextSibling())
-{
-element = node->ToElement();
-obj = new O();
-obj->Deserialize(element);
-objects.push_back(obj);
-}
-}
-return true;
+    node = root->FirstChild(parent.c_str());
+    if (node)
+        {
+            pnode = node->ToElement();
+            for (node = pnode->FirstChild(); node; node=node->NextSibling())
+                {
+                    element = node->ToElement();
+                    obj = new O();
+                    obj->Deserialize(element);
+                    objects.push_back(obj);
+                }
+        }
+    return true;
 }
