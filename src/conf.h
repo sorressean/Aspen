cbc49a462ed86d25457c89e68004085ba4e3cd3e
sorressean <tyler@tysdomain.com>
2015-03-17 18:29:10 +0000

    Updated config

--- src/conf.h
+++ src/conf.h
@@ -20,7 +20,6 @@
 #define GAME_IDLE_TIME (60*60) //the amount of time someone will be able to idle before they are kicked.
 #define LOGIN_IDLE_TIME (30) //the amount of time someone will be able to idle at login.
 #define LINKDEATH_TIME (60*10)
-#define ELAPSED_STARTUP //shows the elapsed time it took the mud to initialize.
 /*
 *Comment this out if you want all scripting functions enabled by default.
 *Leaving it commented will remove functions like SetPassword on player from being exposed to scripting.
