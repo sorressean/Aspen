ca7a428d1b0286c5d10093e688a1120660deaeda
sorressean <tyler@tysdomain.com>
2015-09-27 18:11:52 +0000

    Updated documentation on affect to conform to requirements.

--- src/affect.h
+++ src/affect.h
@@ -1,8 +1,8 @@
 /**
-*Contains: Affect
+* Contains: Affect
 *
-*The affect class is used to add affects to players and npcs.
-*This is done via the Attribute class.
+* The affect class is used to add affects to players and npcs.
+* This is done via the Attribute class.
 */
 
 #pragma once
@@ -13,7 +13,7 @@
 /**
 * Affect is used to apply affects to any living object.
 *
-*It can hold a duration (number of game ticks) as well as the attributes to apply.
+* It can hold a duration (number of game ticks) as well as the attributes to apply.
 * @todo Allow affects to store multiple attributes.
 * @todo allow an ID or some pointer to be set so that affects and their timers can be destroyed when a living leaves the game.
 * @todo add affect removal code on game leave.
