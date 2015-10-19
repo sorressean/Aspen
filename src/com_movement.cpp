22a698ac909d09084378216d0f5c3ca4b33b4c9c
sorressean <tyler@tysdomain.com>
2015-10-19 17:40:11 +0000

    Updated movement commands to only need one class

--- src/com_movement.cpp
+++ src/com_movement.cpp
@@ -11,27 +11,26 @@ void InitializeMovementCommands()
     World* world = World::GetPtr();
 
     world->WriteLog("Initializing movement commands.");
-    world->commands.AddCommand(new CMDNorth());
-    world->commands.AddCommand(new CMDSouth());
-    world->commands.AddCommand(new CMDEast());
-    world->commands.AddCommand(new CMDWest());
-    world->commands.AddCommand(new CMDNortheast());
-    world->commands.AddCommand(new CMDNorthwest());
-    world->commands.AddCommand(new CMDSoutheast());
-    world->commands.AddCommand(new CMDSouthwest());
-    world->commands.AddCommand(new CMDUp());
-    world->commands.AddCommand(new CMDDown());
+    world->commands.AddCommand(new CMDMove(DIR_NORTH));
+    world->commands.AddCommand(new CMDMove(DIR_SOUTH));
+    world->commands.AddCommand(new CMDMove(DIR_EAST));
+    world->commands.AddCommand(new CMDMove(DIR_WEST));
+    world->commands.AddCommand(new CMDMove(DIR_NORTHEAST));
+    world->commands.AddCommand(new CMDMove(DIR_NORTHWEST));
+    world->commands.AddCommand(new CMDMove(DIR_SOUTHEAST));
+    world->commands.AddCommand(new CMDMove(DIR_SOUTHWEST));
+    world->commands.AddCommand(new CMDMove(DIR_UP));
+    world->commands.AddCommand(new CMDMove(DIR_DOWN));
 }
 
-BOOL DoMove(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
+static BOOL DoMove(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
 {
     World* world = World::GetPtr();
     ObjectManager* omanager = world->GetObjectManager();
-
     ExitDirection direction = (ExitDirection)subcmd;
-    Room* room = NULL;
-    Room* dest = NULL;
-    Exit* exit = NULL;
+    Room* room = nullptr;
+    Room* dest = nullptr;
+    Exit* exit = nullptr;
 
     room=(Room*)mobile->GetLocation();
     if (!room)
@@ -39,10 +38,9 @@ BOOL DoMove(const std::string &verb, Player* mobile,std::vector<std::string> &ar
             mobile->Message(MSG_ERROR,"There are no exits here.");
             return false;
         }
-
     if (!room->ExitExists(direction))
         {
-            mobile->Message(MSG_ERROR,"You can't go that way!");
+            mobile->Message(MSG_ERROR,"You can't go that way.");
             return false;
         }
 
@@ -54,134 +52,69 @@ BOOL DoMove(const std::string &verb, Player* mobile,std::vector<std::string> &ar
         }
     if (!mobile->FromRoom())
         {
-            mobile->Message(MSG_ERROR, "You couldn't be moved.");
+            mobile->Message(MSG_ERROR, "Something is preventing you from leaving the room.");
             return false;
         }
 
     dest = omanager->GetRoom(exit->GetTo());
     mobile->MoveTo(dest);
     dest->events.CallEvent("OnEnter", NULL, (void*)dest);
-
-    mobile->Message(MSG_INFO, (omanager->GetRoom(exit->GetTo()))->DoLook(mobile));
+    mobile->Message(MSG_INFO, dest->DoLook(mobile));
     return true;
 }
 
-CMDNorth::CMDNorth()
+CMDMove::CMDMove(int direction)
 {
-    SetName("north");
-    AddAlias("n");
-    SetSubcmd(DIR_NORTH);
+    SetSubcmd(direction);
     SetType(CommandType::Movement);
+    switch(direction)
+        {
+        case DIR_NORTH:
+            SetName("north");
+            AddAlias("n");
+            break;
+        case DIR_SOUTH:
+            SetName("south");
+            AddAlias("s");
+            break;
+        case DIR_EAST:
+            SetName("east");
+            AddAlias("e");
+            break;
+        case DIR_WEST:
+            SetName("west");
+            AddAlias("w");
+            break;
+        case DIR_NORTHEAST:
+            SetName("northeast");
+            AddAlias("ne");
+            break;
+        case DIR_SOUTHEAST:
+            SetName("southeast");
+            AddAlias("se");
+            break;
+        case DIR_SOUTHWEST:
+            SetName("southwest");
+            AddAlias("sw");
+            break;
+        case DIR_NORTHWEST:
+            SetName("northwest");
+            AddAlias("nw");
+            break;
+        case DIR_UP:
+            SetName("up");
+            AddAlias("u");
+            break;
+        case DIR_DOWN:
+            SetName("down");
+            AddAlias("d");
+            break;
+        default:
+            World* world = World::GetPtr();
+            world->WriteLog("Invalid direction specified.", ERR);
+        }
 }
-BOOL CMDNorth::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDSouth::CMDSouth()
-{
-    SetName("south");
-    AddAlias("s");
-    SetSubcmd(DIR_SOUTH);
-    SetType(CommandType::Movement);
-}
-BOOL CMDSouth::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDEast::CMDEast()
-{
-    SetName("east");
-    AddAlias("e");
-    SetSubcmd(DIR_EAST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDEast::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDWest::CMDWest()
-{
-    SetName("west");
-    AddAlias("w");
-    SetSubcmd(DIR_WEST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDWest::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDNortheast::CMDNortheast()
-{
-    SetName("northeast");
-    AddAlias("ne");
-    SetSubcmd(DIR_NORTHEAST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDNortheast::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDSoutheast::CMDSoutheast()
-{
-    SetName("southeast");
-    AddAlias("se");
-    SetSubcmd(DIR_SOUTHEAST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDSoutheast::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDSouthwest::CMDSouthwest()
-{
-    SetName("southwest");
-    AddAlias("sw");
-    SetSubcmd(DIR_SOUTHWEST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDSouthwest::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDNorthwest::CMDNorthwest()
-{
-    SetName("northwest");
-    AddAlias("nw");
-    SetSubcmd(DIR_NORTHWEST);
-    SetType(CommandType::Movement);
-}
-BOOL CMDNorthwest::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDUp::CMDUp()
-{
-    SetName("up");
-    AddAlias("u");
-    SetSubcmd(DIR_UP);
-    SetType(CommandType::Movement);
-}
-BOOL CMDUp::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
-{
-    return DoMove(verb, mobile, args, subcmd);
-}
-
-CMDDown::CMDDown()
-{
-    SetName("down");
-    AddAlias("d");
-    SetSubcmd(DIR_DOWN);
-    SetType(CommandType::Movement);
-}
-BOOL CMDDown::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
+BOOL CMDMove::Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd)
 {
     return DoMove(verb, mobile, args, subcmd);
 }
--- src/com_movement.h
+++ src/com_movement.h
@@ -5,65 +5,11 @@
 #include "command.h"
 
 void InitializeMovementCommands();
-BOOL DoMove(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-class CMDNorth:public Command
+
+class CMDMove:public Command
 {
 public:
-    CMDNorth();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDSouth:public Command
-{
-public:
-    CMDSouth();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDEast:public Command
-{
-public:
-    CMDEast();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDWest:public Command
-{
-public:
-    CMDWest();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDNortheast:public Command
-{
-public:
-    CMDNortheast();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDSoutheast:public Command
-{
-public:
-    CMDSoutheast();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDSouthwest:public Command
-{
-public:
-    CMDSouthwest();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDNorthwest:public Command
-{
-public:
-    CMDNorthwest();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDUp:public Command
-{
-public:
-    CMDUp();
-    BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
-};
-class CMDDown:public Command
-{
-public:
-    CMDDown();
+    CMDMove(int direction);
     BOOL Execute(const std::string &verb, Player* mobile,std::vector<std::string> &args,int subcmd);
 };
 #endif
