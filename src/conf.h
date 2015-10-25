a2551127eb62232141502980858ce1f8eddfebdd
sorressean <tyler@tysdomain.com>
2015-10-25 17:25:58 +0000

    Cleaned up and documented conf.h and configuration values. Moved defines into mud.h to be cleaned up later.

--- src/conf.h
+++ src/conf.h
@@ -1,189 +1,165 @@
 /**
-*configuration defines for the mud
-*Holds configuration and other constants.
+* configuration defines for the mud
+* Holds configuration data and other constants.
+* Do not make changes here.
+* make changes in extensions/conf.h
 */
 #pragma once
 #ifndef CONFIG_H
 #define CONFIG_H
-//Mud configuration constants
+
+/**
+* Should we reverse-lookup connections?
+*/
 #define RDNS
-#define DEFAULT_PORT 6666 //our default port: The mud will use this, if the port isn't provided as an argument.
-#define LISTEN_BACKLOG 5 //the backlog for the listening socket.
-#define PULSES_PER_SECOND 10 //how many game loop itterations will be ran each second
+
+/**
+* The mud default telnet port.
+*/
+#define DEFAULT_PORT 6666
+
+/**
+* The backlog used for listen()
+*/
+#define LISTEN_BACKLOG 5
+
+/**
+* How many pulses will be ran per second?
+* Make sure this is divisible evenly.
+* For example, 1000/10, 1000/2, etc.
+* Change this if you need faster pulses, but understand that it will give slower updates eventually.
+*/
+#define PULSES_PER_SECOND 5
 #define MS_PER_PULSE 1000/PULSES_PER_SECOND
-#define LOG_CONSOLE //will tell the logger to write to console. Comment out if you do not wish this.
-#define LOG_MAXSIZE 1024*1024 //1mb
+
+/**
+* Should we write log messages to console?
+*/
+#define LOG_CONSOLE
+
+/**
+* Max size, in bytes of the log.
+*/
+#define LOG_MAXSIZE 1024*1024
+/**
+* Max amount of files to keep from rotating log.
+*/
 #define LOG_MAXFILES 10 //maximum number of logs to keep.
-#define MIN_LOG_LEVEL (LOG_LEVEL)WARN //the minimum level of logging that will be written to console.
-#define MUD_NAME "Aspen Mud" //name of your mud.
+/**
+* Minimum log level to write to the console.
+*/
+#define MIN_LOG_LEVEL (LOG_LEVEL)WARN
+
+/**
+* The name of the mud.
+*/
+#define MUD_NAME "Aspen Mud"
+
+/**
+* The maximum number of history entries each channel will keep.
+*/
 #define MAX_CHAN_HISTORY_LENGTH 20 //the maximum number of entries channels will log.
-#define GAME_IDLE_TIME (60*60) //the amount of time someone will be able to idle before they are kicked.
-#define LOGIN_IDLE_TIME (30) //the amount of time someone will be able to idle at login.
+
+/**
+* The maximum amount of time in seconds a player can idle before getting kicked.
+*/
+#define GAME_IDLE_TIME (60*60)
+/**
+* The maximum amount of time in seconds a player will be able to idle at login.
+*/
+#define LOGIN_IDLE_TIME 30
+/**
+* the maximum amount of time in seconds a player can be linkdead before being logged out.
+*/
 #define LINKDEATH_TIME (60*10)
-/*
+
+/**
 *Comment this out if you want all scripting functions enabled by default.
-*Leaving it commented will remove functions like SetPassword on player from being exposed to scripting.
+* Leaving it commented will remove functions like Player::SetPassword from being exposed to scripting.
 */
 #define SCRIPT_SECURE
+
+/**
+* Determines whether or not callouts should be profiled.
+*/
 #define PROFILE_CALLOUTS
-//delayed defines
-#define LIVING_PULSE 2 //seconds for each living heartbeat to take place.
-#define WORLD_PULSE 10 //the world pulse, used for room resets, etc.
-#define SAVE_INTERVAL 180 //90 seconds.
-#define BACKUP_INTERVAL 300 //2 minutes.
+
+/**
+* Pulse living every x seconds.
+*/
+#define LIVING_PULSE 2
+/**
+* Pulse world every x seconds.
+*/
+#define WORLD_PULSE 60
+
+/**
+* How frequently to save players.
+*/
+#define SAVE_INTERVAL (60*5)
+/**
+* Amount of time before backing up the player's files.
+*/
+#define BACKUP_INTERVAL (60*30)
+
 //Defines for locating files:
 #define MOTD_FILE "../txt/motd" //message of the day shown after login
 #define LOGIN_FILE "../txt/welcome" //banner shown at login
 #define COPYOVER_FILE "../copyover.dat" //stores player connection info
 #define BIN_FILE "../bin/aspen" //used for copyovers.
-#define SETTINGS_FILE "../data/mud.dat"
 #define EVENT_FILE "../logs/events.log" //our main log file
 #define EVENT_NAME "events" //the actual name of the event log.
 #define SCRIPT_FILE "../logs/script.log" //the name of the scripting log file.
 #define SCRIPT_NAME "script"
 #define HELP_FILE "../data/help.dat"
 #define SOCIALS_FILE "../data/socials.dat"
+
 //directories:
 #define PLAYER_DIR "../data/players/"
 #define STATE_DIR "../data/state/"
 #define BACKUP_DIR "../data/backups/"
 #define AREA_DIR "../data/areas/"
 #define AREA_STARTFILE "../data/areas/start"
-//player rankings:
-#define RANK_PLAYER 1<<1
-#define RANK_PLAYTESTER 1<<2
-#define RANK_NEWBIEHELPER 1<<3
-#define RANK_NEWBIE 1<<4
-#define RANK_BUILDER 1<<5
-#define RANK_ADMIN 1<<29
-#define RANK_GOD 1<<30
-//flags
-//player flags
-#define PF_SILENCE 1<<1 //player can't broadcast
-#define FKILLER 1<<2 //player is PK
-#define PF_FROZEN 1<<3 //player can't do anything.
-#define PF_NOTITLE 1<<4 //player can't set a title
-//room flags
-#define RF_NOSAY 1<<1
-#define RF_DARK 1<<2
-#define RF_DEATH 1<<3
-#define RF_NOMOB 1<<4
-#define RF_INDOORS 1<<5
-#define RF_PEACE 1<<6
-#define RF_PRIVATE 1<<7
-#define RF_HOUSE 1<<8
-//NPC flags
-#define MF_STATIC 1<<1 //mob does not move
-#define MF_SCAVENGER 1<<2 //mob picks up eq on the ground.
-#define M_AGGRESSIVE 1<<3 //mob is aggro
-#define MF_ZONELOCK 1<<4 //mob doesn't leave zone
-#define MF_WIMPY 1<<5 //mob flees when it gets hurt.
-#define MF_MEMORY 1<<6 //remember attackers.
-#define MF_ASSIST 1<<7 //assist other mobs fighting.
-#define MF_NOKILL 1<<8 //mob can't be killed.
 
-#define VNUMKEEP 100 //how many spare vnums to store.
-//logger levels
-enum LOG_LEVEL {INFORM,WARN,ERR,CRIT,SCRIPT, PLAYER, CONNECTION};
-//Message types:
-enum MessageType {MSG_NORMAL, MSG_ERROR,MSG_INFO,MSG_CRITICAL,MSG_CHANNEL,MSG_LIST};
+/**
+* VNums can be stored to keep track of unused numbers.
+* this makes it quicker to allocate static objects and rooms.
+* If memory is a concern (multiple zones, etc) consider reducing this if you do not have frequent use of allocations
+* Which would require the search for a free VNUM.
+* @todo add the ability to refill at a certain level (so 50% of capacity).
+*/
+#define VNUMKEEP 100
 
-//terrain types:
-enum class TERRAIN
-{
-    OCEAN, UNDERWATER, AIR, PLANE, DESERT, UNDERGROUND, SWAMP, HILLS, MOUNTAIN, FOREST, LIGHTFOREST, CITY, INSIDE, MAX
-};
-//positions:
-enum class POSITION
-{
-    any, unconcious, sleeping, sitting, standing, flying
-};
-//exit/room constants:
-#define EXIT_NOWHERE -1
-#define ROOM_NOWHERE -1
-#define ROOM_START 1
-//Direction defines for subcmd.
-#define DIR_NORTH 1
-#define DIR_SOUTH 2
-#define DIR_EAST 3
-#define DIR_WEST 4
-#define DIR_NORTHEAST 5
-#define DIR_NORTHWEST 6
-#define DIR_SOUTHEAST 7
-#define DIR_SOUTHWEST 8
-#define DIR_UP 9
-#define DIR_DOWN 10
-enum ExitDirection {nowhere, north, south, east, west, northeast, northwest, southeast, southwest, up, down};
-//colors:
-#define C_NORMAL "\x1B[0m"
-#define C_RED "\x1B[31m"
-#define C_GREEN "\x1B[32m"
-#define C_YELLOW "\x1B[33m"
-#define C_BLUE "\x1B[34m"
-#define C_MAGENTA "\x1B[35m"
-#define C_CYAN "\x1B[36m"
-#define C_WHITE "\x1B[37m"
-#define C_RESET "\033[0m"
-//bold
-#define CB_RED "\x1B[1;31m"
-#define CB_GREEN "\x1B[1;32m"
-#define CB_YELLOW "\x1B[1;33m"
-#define CB_BLUE "\x1B[1;34m"
-#define CB_MAGENTA "\x1B[1;35m"
-#define CB_CYAN "\x1B[1;36m"
-#define CB_WHITE "\x1B[1;37m"
-
+/**
+* Maximum password length.
+*/
 #define MAX_PASSWORD 20
+/**
+* Minimum password length.
+*/
 #define MIN_PASSWORD 5
-//module defines
-//below, define all the modules you want to be compiled in.
+
+/**
+* @todo remove these checks.
+* These modules (with the exception of scripting and possibly help ) are required.
+*/
 #define MODULE_SCRIPTING
 #define MODULE_SYSLOG
 #define MODULE_HELP
 #define OLC
-//Leave this commented if you want the code to check for files and create them if they do not exist. see socials.cpp and zone.cpp for examples.
-//#define NO_INIT_DEFAULTS
-//msdp defines.
-#define MSDP_VAR                       1
-#define MSDP_VAL                       2
-#define MSDP_TABLE_OPEN                3
-#define MSDP_TABLE_CLOSE               4
-#define MSDP_ARRAY_OPEN                5
-#define MSDP_ARRAY_CLOSE               6
-#define MAX_MSDP_SIZE                  100
-//MSDP indexes
 
-#define MSDP_CHARNAME 0
-#define MSDP_SERVERTIME 1
-#define MSDP_AFFECTS 2
-#define MSDP_EXPERIENCE 3
-#define MSDP_MAXEXPERIENCE 4
-#define MSDP_TNL 5
-#define MSDP_HEALTH 6
-#define MSDP_MAXHEALTH 7
-#define MSDP_LEVEL 8
-#define MSDP_MONEY 9
-#define MSDP_STR 10
-#define MSDP_DEX 11
-#define MSDP_CON 12
-#define MSDP_INT 13
-#define MSDP_PER 14
-#define MSDP_BASESTR 15
-#define MSDP_BASEDEX 16
-#define MSDP_BASECON 17
-#define MSDP_BASEINT 18
-#define MSDP_BASEPER 19
-#define MSDP_OPPONENTHEALTH 20
-#define MSDP_MAXOPPONENTHEALTH 21
-#define MSDP_OPPONENTNAME 22
-#define MSDP_AREA 23
-#define MSDP_EXITS 24
-#define MSDP_ROOM 25
-#define MSDP_RVNUM 29
-#define MSDP_MAX 30
-#define VERSION_STR "Sapphire Mud v0.1.1"
-#define MAX_LEVEL 251
-#define MAX_MOB_LEVEL 301
+/**
+* Leave this commented if you would like initialization to check for files and create them upon launch.
+* Only useful for first-time launch.
+*/
+//#define NO_INIT_DEFAULTS
+
+/**
+* How many bytes to allocate for MCCP buffers for individual sockets.
+* Do not change unless you have a good reason.
+*/
 #define MCCP_BUFFER_SIZE 4096
+
+//include mud-specific configuration data
 #include "extensions/conf.h"
 #endif
--- src/mud.h
+++ src/mud.h
@@ -1,22 +1,36 @@
-//mud constants and values:
+/**
+* Contains: mud constants and values.
+*/
 #pragma once
 #ifndef MUD_H
 #define MUD_H
 #include "conf.h"
+/**
+* @todo move to utils.h
+*/
 //used to show where an exception was thrown:
 #define WHERE() std::string(" In ")+std::string(__FUNCTION__)+std::string(" at ")+std::string(__FILE__)+std::string(".")
+
 //typedefs:
 typedef int FLAG; //bitfield flags
 typedef int VNUM;
 typedef int BOOL;
+/**
+* @todo clean some of these up.
+*/
 typedef unsigned int UINT;
 typedef unsigned int DWORD;
 typedef unsigned long long int OBJID;
+
 struct range
 {
     int min;
     int max;
 };
+
+/**
+* @todo remove these or as many as possible, put forward declarations in the files that require them.
+*/
 //forward declarations
 //I wanted to avoid these, but it seems I have no choice
 class World;
@@ -32,10 +46,133 @@ class Zone;
 class Room;
 class ObjectManager;
 
-struct OLC_DATA;
-
 //olc forward declarations
 struct OLC_DATA;
-//enum OLC_INPUT_TYPE {NUM, STRING, BOOLEAN, NOINPUT, EDITOR};
-//typedef void (*FP_INPUT)(Entity*, Player*, const Variant*);
+
+//player rankings:
+#define RANK_PLAYER 1<<1
+#define RANK_PLAYTESTER 1<<2
+#define RANK_NEWBIEHELPER 1<<3
+#define RANK_NEWBIE 1<<4
+#define RANK_BUILDER 1<<5
+#define RANK_ADMIN 1<<29
+#define RANK_GOD 1<<30
+
+//player flags
+#define PF_SILENCE 1<<1 //player can't broadcast
+#define FKILLER 1<<2 //player is PK
+#define PF_FROZEN 1<<3 //player can't do anything.
+#define PF_NOTITLE 1<<4 //player can't set a title
+//room flags
+#define RF_NOSAY 1<<1
+#define RF_DARK 1<<2
+#define RF_DEATH 1<<3
+#define RF_NOMOB 1<<4
+#define RF_INDOORS 1<<5
+#define RF_PEACE 1<<6
+#define RF_PRIVATE 1<<7
+#define RF_HOUSE 1<<8
+//NPC flags
+#define MF_STATIC 1<<1 //mob does not move
+#define MF_SCAVENGER 1<<2 //mob picks up eq on the ground.
+#define M_AGGRESSIVE 1<<3 //mob is aggro
+#define MF_ZONELOCK 1<<4 //mob doesn't leave zone
+#define MF_WIMPY 1<<5 //mob flees when it gets hurt.
+#define MF_MEMORY 1<<6 //remember attackers.
+#define MF_ASSIST 1<<7 //assist other mobs fighting.
+#define MF_NOKILL 1<<8 //mob can't be killed.
+
+//logger levels
+enum LOG_LEVEL {INFORM,WARN,ERR,CRIT,SCRIPT, PLAYER, CONNECTION};
+//Message types:
+enum MessageType {MSG_NORMAL, MSG_ERROR,MSG_INFO,MSG_CRITICAL,MSG_CHANNEL,MSG_LIST};
+
+//terrain types:
+enum class TERRAIN
+{
+    OCEAN, UNDERWATER, AIR, PLANE, DESERT, UNDERGROUND, SWAMP, HILLS, MOUNTAIN, FOREST, LIGHTFOREST, CITY, INSIDE, MAX
+};
+
+enum class POSITION
+{
+    any, unconcious, sleeping, sitting, standing, flying
+};
+//exit/room constants:
+#define EXIT_NOWHERE -1
+#define ROOM_NOWHERE -1
+#define ROOM_START 1
+
+//Direction defines for subcmd.
+#define DIR_NORTH 1
+#define DIR_SOUTH 2
+#define DIR_EAST 3
+#define DIR_WEST 4
+#define DIR_NORTHEAST 5
+#define DIR_NORTHWEST 6
+#define DIR_SOUTHEAST 7
+#define DIR_SOUTHWEST 8
+#define DIR_UP 9
+#define DIR_DOWN 10
+
+enum ExitDirection
+{
+    nowhere, north, south, east, west, northeast, northwest, southeast, southwest, up, down
+};
+//colors:
+#define C_NORMAL "\x1B[0m"
+#define C_RED "\x1B[31m"
+#define C_GREEN "\x1B[32m"
+#define C_YELLOW "\x1B[33m"
+#define C_BLUE "\x1B[34m"
+#define C_MAGENTA "\x1B[35m"
+#define C_CYAN "\x1B[36m"
+#define C_WHITE "\x1B[37m"
+#define C_RESET "\033[0m"
+//bold
+#define CB_RED "\x1B[1;31m"
+#define CB_GREEN "\x1B[1;32m"
+#define CB_YELLOW "\x1B[1;33m"
+#define CB_BLUE "\x1B[1;34m"
+#define CB_MAGENTA "\x1B[1;35m"
+#define CB_CYAN "\x1B[1;36m"
+#define CB_WHITE "\x1B[1;37m"
+
+//msdp defines.
+#define MSDP_VAR                       1
+#define MSDP_VAL                       2
+#define MSDP_TABLE_OPEN                3
+#define MSDP_TABLE_CLOSE               4
+#define MSDP_ARRAY_OPEN                5
+#define MSDP_ARRAY_CLOSE               6
+#define MAX_MSDP_SIZE                  100
+//MSDP indexes
+
+#define MSDP_CHARNAME 0
+#define MSDP_SERVERTIME 1
+#define MSDP_AFFECTS 2
+#define MSDP_EXPERIENCE 3
+#define MSDP_MAXEXPERIENCE 4
+#define MSDP_TNL 5
+#define MSDP_HEALTH 6
+#define MSDP_MAXHEALTH 7
+#define MSDP_LEVEL 8
+#define MSDP_MONEY 9
+#define MSDP_STR 10
+#define MSDP_DEX 11
+#define MSDP_CON 12
+#define MSDP_INT 13
+#define MSDP_PER 14
+#define MSDP_BASESTR 15
+#define MSDP_BASEDEX 16
+#define MSDP_BASECON 17
+#define MSDP_BASEINT 18
+#define MSDP_BASEPER 19
+#define MSDP_OPPONENTHEALTH 20
+#define MSDP_MAXOPPONENTHEALTH 21
+#define MSDP_OPPONENTNAME 22
+#define MSDP_AREA 23
+#define MSDP_EXITS 24
+#define MSDP_ROOM 25
+#define MSDP_RVNUM 29
+#define MSDP_MAX 30
 #endif
