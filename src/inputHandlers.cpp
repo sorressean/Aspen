30329172977e6d28e715cd30097720d5faf838b6
sorressean <tyler@tysdomain.com>
2015-10-28 17:49:47 +0000

    Updated code for input handlers, also documented.

--- src/inputHandlers.cpp
+++ src/inputHandlers.cpp
@@ -1,13 +1,11 @@
 #include "mud.h"
-#include "conf.h"
 #include "socket.h"
 #include "utils.h"
 #include "inputHandlers.h"
 
-YesNoHandler::YesNoHandler(YESNOCB cb, void* arg):InputHandle()
+YesNoHandler::YesNoHandler(YESNOCB cb, void* arg):
+    InputHandle(), _cb(cb), _arg(arg)
 {
-    _cb = cb;
-    _arg = arg;
 }
 
 void YesNoHandler::Input(void* arg, const std::string &input)
@@ -18,38 +16,30 @@ void YesNoHandler::Input(void* arg, const std::string &input)
         {
             _cb((Socket*)arg, true, _arg);
         }
+
     _cb((Socket*)arg, false, _arg);
 }
 
 BOOL YesNoHandler::CreateHandler(Socket* sock,  YESNOCB cb, void* arg)
 {
-    if (sock == NULL)
+    in_data* input = nullptr;
+    YesNoHandler* handle  = nullptr;
+
+    if (sock == nullptr)
         {
             return false;
         }
 
-    in_data* input = new in_data();
-    if (!input)
-        {
-            return false;
-        }
-
-    YesNoHandler* handle = new YesNoHandler(cb, arg);
-    if (!handle)
-        {
-            delete input;
-            return false;
-        }
-
+    input = new in_data();
+    handle = new YesNoHandler(cb, arg);
     input->handle = handle;
     input->args = (void*)sock;
     return sock->SetInput(input);
 }
 
-LineHandler::LineHandler(LINECB cb, void* arg):InputHandle()
+LineHandler::LineHandler(LINECB cb, void* arg):
+    InputHandle(), _cb(cb), _arg(arg)
 {
-    _cb = cb;
-    _arg = arg;
 }
 
 void LineHandler::Input(void* arg, const std::string &input)
@@ -60,43 +50,34 @@ void LineHandler::Input(void* arg, const std::string &input)
 
 BOOL LineHandler::CreateHandler(Socket* sock,  LINECB cb)
 {
-    if (sock == NULL)
+    in_data* input = nullptr;
+    LineHandler* handle = nullptr;
+
+    if (sock == nullptr)
         {
             return false;
         }
 
-    in_data* input = new in_data();
-    if (!input)
-        {
-            return false;
-        }
-
-    LineHandler* handle = new LineHandler(cb);
-    if (!handle)
-        {
-            delete input;
-            return false;
-        }
-
+    input = new in_data();
+    handle = new LineHandler(cb);
     input->handle = handle;
     input->args = (void*)sock;
     return sock->SetInput(input);
 }
 
-TextBlockHandler::TextBlockHandler(TEXTBLOCKCB cb,  std::vector<std::string>* lines, void* arg)
+TextBlockHandler::TextBlockHandler(TEXTBLOCKCB cb,  std::vector<std::string>* lines, void* arg):
+    InputHandle(), _cb(cb), _lines(lines), _arg(arg)
 {
-    _arg = arg;
-    _cb = cb;
-    _lines = lines;
 }
+
 void TextBlockHandler::Input(void* arg, const std::string &input)
 {
     if (input == ".")
         {
-            std::string data = input;
             _cb((Socket*)arg, _lines, _arg);
             return;
         }
+
     if (_lines)
         {
             _lines->push_back(input);
@@ -105,24 +86,16 @@ void TextBlockHandler::Input(void* arg, const std::string &input)
 
 BOOL TextBlockHandler::CreateHandler(Socket* sock, TEXTBLOCKCB cb, std::vector<std::string>* lines, void* arg)
 {
-    if (sock == NULL || lines == NULL)
+    in_data* input = nullptr;
+    TextBlockHandler* handle = nullptr;
+
+    if (sock == nullptr || lines == nullptr)
         {
             return false;
         }
 
-    in_data* input = new in_data();
-    if (!input)
-        {
-            return false;
-        }
-
-    TextBlockHandler* handle = new TextBlockHandler(cb, lines, arg);
-    if (!handle)
-        {
-            delete input;
-            return false;
-        }
-
+    input = new in_data();
+    handle = new TextBlockHandler(cb, lines, arg);
     input->handle = handle;
     input->args = (void*)sock;
     return sock->SetInput(input);
--- src/inputHandlers.h
+++ src/inputHandlers.h
@@ -1,40 +1,95 @@
-/*
-*These are basic utility classes, that create generic input handlers.
+/**
+* Contains: various input handlers.
+*
+* Input handlers are used when you wish to request player input.
+* They receive all input from the socket.
 */
 #pragma once
 #ifndef INPUT_HANDLERS_H
 #define INPUT_HANDLERS_H
-#include "mud.h"
-#include "conf.h"
-#include "socket.h"
 #include <functional>
+#include "mud.h"
+#include "socket.h"
+
+/**
+* The callback for a yes/no handler.
+*
+* @param [in] the socket associated with the handler.
+* @param [in] true if yes was chosen, false otherwise.
+* @param [in] associated args.
+*/
 typedef std::function<void (Socket*, BOOL, void*)> YESNOCB;
+/**
+* Line input callback.
+*
+* @param [in] the associated socket.
+* @param [in] the provided line of input.
+* @param [in] associated args.
+*/
 typedef std::function<void (Socket*, std::string&, void*)> LINECB;
 typedef std::function<void (Socket*, std::vector<std::string>*, void*)> TEXTBLOCKCB;
 
+/**
+* This handler is used to provide a yes/no question to the player.
+*/
 class YesNoHandler:public InputHandle
 {
     YESNOCB _cb;
     void* _arg;
 public:
+    /**
+    * Constructs an input handler.
+    *
+    * @param [in] cb the callback to fire on comletion.
+    * @param [in] args associated args with this handler.
+    */
     YesNoHandler(YESNOCB cb, void* arg=NULL);
+    /**
+    * @copydoc InputHandle::Input
+    */
     void Input(void* arg, const std::string &input);
-    static BOOL CreateHandler(Socket* sock, YESNOCB cb, void* arg = NULL);
+    /**
+    * Creates this handle and handles attachment.
+    *
+    * @param [in] sock the socket associated with the handle.
+    * @param [in] cb the callback.
+    * @param [in] args associated arguments.
+    */
+    static BOOL CreateHandler(Socket* sock, YESNOCB cb, void* arg = nullptr);
 };
 
+/**
+* This handler presents a line input for the player.
+*
+* Use for requesting a line of input such as a pet name, etc.
+*/
 class LineHandler:public InputHandle
 {
     LINECB _cb;
     void* _arg;
 public:
+    /**
+    * Constructs an input handler.
+    *
+    * @param [in] cb the callback to fire on comletion.
+    * @param [in] args associated args with this handler.
+    */
     LineHandler(LINECB cb, void* arg = NULL);
+    /**
+    * @copydoc InputHandle::Input
+    */
     void Input(void* arg, const std::string &input);
+    /**
+    * Creates this handle and handles attachment.
+    *
+    * @param [in] sock the socket associated with the handle.
+    * @param [in] cb the callback.
+    */
     static BOOL CreateHandler(Socket* sock, LINECB cb);
 };
 
-/*
-*Our textblock handler, used for receiving large amounts of text, such as a message,
-*multiple lines in an editor, etc.
+/**
+* This handler is used for receiving a large block of text or multiple lines.
 */
 class TextBlockHandler:public InputHandle
 {
@@ -42,8 +97,26 @@ class TextBlockHandler:public InputHandle
     std::vector<std::string>* _lines;
     void* _arg;
 public:
+    /**
+    * Constructs an input handler.
+    *
+    * @param [in] cb the callback to fire on comletion.
+    * @param [in] lines a pointer to a vector which will receive the lines from the handler.
+    * @param [in] args associated args with this handler.
+    */
     TextBlockHandler(TEXTBLOCKCB cb,  std::vector<std::string>* lines, void* arg = NULL);
+    /**
+    * @copydoc InputHandle::Input
+    */
     void Input(void* arg, const std::string &input);
-    static BOOL CreateHandler(Socket* sock, TEXTBLOCKCB cb, std::vector<std::string>* lines, void* arg = NULL);
+    /**
+    * Creates this handle and handles attachment.
+    *
+    * @param [in] sock the socket associated with the handle.
+    * @param [in] cb the callback.
+    * @param [in] lines a pointer to the vector that received the input.
+    * @param [in] args the args associated with the handler.
+    */
+    static BOOL CreateHandler(Socket* sock, TEXTBLOCKCB cb, std::vector<std::string>* lines, void* arg = nullptr);
 };
 #endif
