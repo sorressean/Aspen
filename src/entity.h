ddf625c230818ab954caf011bca93e03d52b41bc
sorressean <tyler@tysdomain.com>
2016-04-13 16:44:30 +0000

    Revert "Updated event code."
    This reverts commit 2875ae693e1a4637f40c9901f8b84fb76db183ed.

--- src/entity.h
+++ src/entity.h
@@ -26,9 +26,10 @@ class Entity:public ObjectContainer
 protected:
     ObjectContainer* _location;
     StaticObject* _parent;
+    Uuid _uuid;
     std::vector<std::string> _aliases;
+
     std::string _short;
-Uuid _uuid;
 public:
     Entity();
     virtual ~Entity() = default;
